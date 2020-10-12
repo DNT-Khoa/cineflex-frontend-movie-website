50858baea9eb81a12402620ed50cc5241a0dbcc9
Khoa Doan <dntkhoa@gmail.com>
2020-10-12 17:22:01 +0200

    Finish improving the signup form frontend

--- cineflex-frontend/src/app/auth/signup/signup.component.ts
+++ cineflex-frontend/src/app/auth/signup/signup.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
@@ -6,10 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.component.scss']
 })
 export class SignupComponent implements OnInit {
+  isPasswordVisible = false;
+  signupForm: FormGroup
 
   constructor() { }
 
   ngOnInit(): void {
+    this.signupForm = new FormGroup({
+      firstName: new FormControl('', Validators.required),
+      lastName: new FormControl('', Validators.required),
+      email: new FormControl('', [Validators.email, Validators.required]),
+      password: new FormControl('', Validators.required)
+    })
   }
 
 }
