433f19a59db0ac071c5e0285fbb6d69abeac7b45
Khoa Doan <dntkhoa@gmail.com>
2020-10-12 06:02:52 +0200

    Finish the styling for the sign up and login styling

--- cineflex-frontend/src/app/app-routing.module.ts
+++ cineflex-frontend/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./home/home.module').then(m => m.HomeModule),
+    pathMatch: 'full'
+  },
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- cineflex-frontend/src/app/home/header/header.component.ts
+++ cineflex-frontend/src/app/home/header/header.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { trigger, state, style, transition, animate } from '@angular/animations';
+import { trigger, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-header',
