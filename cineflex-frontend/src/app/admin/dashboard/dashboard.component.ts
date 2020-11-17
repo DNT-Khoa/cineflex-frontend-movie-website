0ae516e232e1348e6a90d4236582ff5da862a8a7
Khoa Doan <dntkhoa@gmail.com>
2020-11-17 15:36:03 +0100

    Finish implementing the dashboard cards for frontend

--- cineflex-frontend/src/app/admin/dashboard/dashboard.component.ts
+++ cineflex-frontend/src/app/admin/dashboard/dashboard.component.ts
@@ -7,17 +7,47 @@ import { DashboardService } from './shared/dashboard.service';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
-
+  numOfUsers: number;
+  numOfMovies: number;
+  numOfPosts: number;
+  numOfComments: number;
   constructor(private dashboardService: DashboardService) { }
 
   ngOnInit(): void {
     this.getCountAllUsers();
+    this.getCountAllComments();
+    this.getCountAllMovies();
+    this.getCountAllPosts();
   }
 
   getCountAllUsers() {
     this.dashboardService.getNumberOfTotalUsers().subscribe(
       data => {
-        console.log(data);
+        this.numOfUsers = data;
+      }
+    )
+  }
+
+  getCountAllMovies() {
+    this.dashboardService.getNumberOfTotalMovies().subscribe(
+      data => {
+        this.numOfMovies = data;
+      }
+    )
+  }
+
+  getCountAllPosts() {
+    this.dashboardService.getNumberOfTotalPosts().subscribe(
+      data => {
+        this.numOfPosts = data;
+      }
+    )
+  }
+
+  getCountAllComments() {
+    this.dashboardService.getNumberOfTotalComments().subscribe(
+      data => {
+        this.numOfComments = data;
       }
     )
   }
--- cineflex-frontend/src/app/admin/dashboard/shared/dashboard.service.ts
+++ cineflex-frontend/src/app/admin/dashboard/shared/dashboard.service.ts
@@ -12,4 +12,18 @@ export class DashboardService {
     getNumberOfTotalUsers() {
         return this.httpClient.get<number>(this.httpConfigService.getBaseUrl() + "/admin/countUsers");
     }
+
+    getNumberOfTotalMovies() {
+        return this.httpClient.get<number>(this.httpConfigService.getBaseUrl() + "/admin/countMovies");
+    }
+
+    getNumberOfTotalPosts() {
+        return this.httpClient.get<number>(this.httpConfigService.getBaseUrl() + "/admin/countPosts");
+    }
+
+    getNumberOfTotalComments() {
+        return this.httpClient.get<number>(this.httpConfigService.getBaseUrl() + "/admin/countComments");
+    }
+
+
 }
