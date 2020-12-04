4621d7855d8466e06361227f41feaeb84047c281
Khoa Doan <dntkhoa@gmail.com>
2020-12-04 18:56:11 +0100

    Deleted ready made test files for all components

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,16 @@
-// require('dotenv').config();
-// const enablePurge = process.env.ENABLE_PURGE || false;
+require('dotenv').config();
+const enablePurge = process.env.ENABLE_PURGE || false;
 module.exports = {
   future: {
-    // removeDeprecatedGapUtilities: true,
-    // purgeLayersByDefault: true,
+    removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true,
   },
   purge: {
-    // enabled: enablePurge,
-    // content: [
-    //   './src/**/*.html',
-    //   './src/**/*.scss'
-    // ]
+    enabled: enablePurge,
+    content: [
+      './src/**/*.html',
+      './src/**/*.scss'
+    ]
   },
   theme: {
     extend: {
